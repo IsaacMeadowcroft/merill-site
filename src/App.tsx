fa72f926664e1b6a9cdfd067408affe0a0ac705a
IsaacMeadowcroft <54515271+IsaacMeadowcroft@users.noreply.github.com>
2021-10-04 18:29:00 -0400

    First step

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Button, Image, Offcanvas } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import LandingPage from './LandingPage.jpg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{backgroundColor: 'black'}}>
+        <Image src={LandingPage} fluid className="float-left"/>
+        <div className="v75_413">
+          <span className="v75_414">
+            Visual Artist
+          </span>
+          <span className="v75_415">
+            MERILL BOBOTIS
+          </span>
+          <div className="name"></div>
+        </div>
+        <Button>Test Button</Button>
+        <SideBar/>
     </div>
   );
 }
+
+function SideBar() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow} className="me-2"/>
+      <Offcanvas show={show} onHide={handleClose} placement={"end"}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          Some text as placeholder. In real life you can have the elements you
+          have chosen. Like, text, images, lists, etc.
+        </Offcanvas.Body>
+      </Offcanvas>
+    </>
+  );
+}
+
 
 export default App;
