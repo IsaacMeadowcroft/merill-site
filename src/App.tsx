6973340cb521eba305df14c58e1c3397a8d1cd0b
IsaacMeadowcroft <54515271+IsaacMeadowcroft@users.noreply.github.com>
2021-10-04 21:44:45 -0400

    Home page draft

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,76 @@
 import React, { useState } from 'react';
 import './App.css';
-import { Button, Image, Offcanvas } from 'react-bootstrap'
+import { Button, Col, Container, Image, Offcanvas, Row, Stack } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import LandingPage from './LandingPage.jpg'
 
 function App() {
   return (
-    <div style={{backgroundColor: 'black'}}>
-        <Image src={LandingPage} fluid className="float-left"/>
-        <div className="v75_413">
-          <span className="v75_414">
-            Visual Artist
-          </span>
-          <span className="v75_415">
+    <HomePage />
+  )
+}
+
+function HomePage() {
+  return (
+    <Container fluid style={{backgroundColor: 'black', padding: '0px'}}>
+      <Row xs={1} sm={1} md={3}>
+        <Col md={4}>
+          <Image src={LandingPage} fluid />
+        </Col>
+        <Col md={4}>
+          <Col className="name">
             MERILL BOBOTIS
-          </span>
-          <div className="name"></div>
-        </div>
-        <Button>Test Button</Button>
-        <SideBar/>
-    </div>
+          </Col>
+          <Col className="bar" />
+          <Col className="profession">
+            Visual Artist
+          </Col>
+        </Col>
+        <Col md={1}>
+          <SideBar/>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
 function SideBar() {
   const [show, setShow] = useState(false);
-
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
   return (
     <>
-      <Button variant="primary" onClick={handleShow} className="me-2"/>
-      <Offcanvas show={show} onHide={handleClose} placement={"end"}>
-        <Offcanvas.Header closeButton>
-          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
-        </Offcanvas.Header>
-        <Offcanvas.Body>
-          Some text as placeholder. In real life you can have the elements you
-          have chosen. Like, text, images, lists, etc.
+      <Button onClick={handleShow} variant="outline-dark" className="sidebar-toggle"/>
+      <Offcanvas show={show} onHide={handleClose} placement={"end"} style={{backgroundColor: "black", borderColor: "white", borderRight: "1px"}}>
+        <Offcanvas.Header closeButton />
+        <Offcanvas.Body style={{padding: "0px", backgroundColor: "black"}}>
+          <MenuItems />
         </Offcanvas.Body>
       </Offcanvas>
     </>
+  );
+}
+
+function MenuItems() {
+  return (
+    <Stack gap={3}>
+      <Container>
+        <a href="#link" className="sidebar-button">Home</a>
+      </Container>
+      <Container>
+        <a href="#link" className="sidebar-button">Prints</a>
+      </Container>
+      <Container>
+        <a href="#link" className="sidebar-button">Portfolio</a>
+      </Container>
+      <Container>
+        <a href="#link" className="sidebar-button">About</a>
+      </Container>
+      <Container>
+        <a href="#link" className="sidebar-button">Contact</a>
+      </Container>
+    </Stack>
   );
 }
 
