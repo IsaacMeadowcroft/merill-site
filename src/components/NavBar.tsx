59f6ce57232eb84cb8f890e699d05854412b086b
IsaacMeadowcroft <54515271+IsaacMeadowcroft@users.noreply.github.com>
2021-10-06 18:54:38 -0400

    Formated NavBar nicely!

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -22,17 +22,13 @@ function NavBar(): JSX.Element {
   }, []);
   
   return (
-    <Navbar bg="dark" variant="dark">
-      <Container>
-        <Nav className={ dimensions.width <= 421? "me-auto nav-styles-sm" : "me-auto nav-styles-md" }>
-          <Nav.Link href="#Home">HOME</Nav.Link>
-          <Nav.Link href="#Prints">PRINTS</Nav.Link>
-          <Nav.Link href="#Portfolio">PORTFOLIO</Nav.Link>
-          <Nav.Link href="#About">ABOUT</Nav.Link>
-          <Nav.Link href="#Contact">CONTACT</Nav.Link>
+        <Nav fill justify className={ dimensions.width <= 429? "me-auto nav-styles-sm" : "me-auto nav-styles-md" }>
+          <Nav.Link  href="#Home">HOME</Nav.Link>
+          <Nav.Link  href="#Prints">PRINTS</Nav.Link>
+          <Nav.Link  href="#Portfolio">PORTFOLIO</Nav.Link>
+          <Nav.Link  href="#About">ABOUT</Nav.Link>
+          <Nav.Link  href="#Contact">CONTACT</Nav.Link>
         </Nav>
-      </Container>
-    </Navbar>
   );
 }
 
