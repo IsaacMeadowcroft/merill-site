24b9ed197a4fddd9c3196b55754a9d0629da6952
IsaacMeadowcroft <54515271+IsaacMeadowcroft@users.noreply.github.com>
2021-10-06 17:08:40 -0400

    Working Nav

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../css/About.css";
+import "../css/NavBar.css";
 import { Container, Nav, Navbar } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -7,12 +7,12 @@ function NavBar(): JSX.Element {
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
-        <Nav className="me-auto" >
-          <Nav.Link href="#Home">Home</Nav.Link>
-          <Nav.Link href="#Prints">Prints</Nav.Link>
-          <Nav.Link href="#Portfolio">Portfolio</Nav.Link>
-          <Nav.Link href="#About">About</Nav.Link>
-          <Nav.Link href="#Contact">Contact</Nav.Link>
+        <Nav className="me-auto nav-styles" >
+          <Nav.Link href="#Home">HOME</Nav.Link>
+          <Nav.Link href="#Prints">PRINTS</Nav.Link>
+          <Nav.Link href="#Portfolio">PORTFOLIO</Nav.Link>
+          <Nav.Link href="#About">ABOUT</Nav.Link>
+          <Nav.Link href="#Contact">CONTACT</Nav.Link>
         </Nav>
       </Container>
     </Navbar>
