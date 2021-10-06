338bf28c5edeb71ccd0c9e9336422b6810523328
IsaacMeadowcroft <54515271+IsaacMeadowcroft@users.noreply.github.com>
2021-10-06 15:50:14 -0400

    Fixed window resize point

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,7 +5,7 @@ import SideBar from "./SideBar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import LandingPage from "../assets/LandingPage.jpg";
 
-function Home(): JSX.Element {
+function Home(): JSX.Element{
   const [dimensions, setDimensions] = React.useState({ 
     height: window.innerHeight,
     width: window.innerWidth
@@ -22,25 +22,25 @@ function Home(): JSX.Element {
     window.addEventListener('resize', handleResize);
   }, []);
     
-  if (dimensions.width >= 576) {
+  if (dimensions.width >= 768) {
     return (
       <Container
         fluid
         style={{ backgroundColor: "black", padding: "0px" }}
         id="Home"
       >
-        <Row sm={3} style={{ margin: "0px" }}>
-          <Col sm={5} style={{ padding: "0px" }}>
+        <Row md={3} style={{ margin: "0px" }}>
+          <Col md={5} style={{ padding: "0px" }}>
             <Image src={LandingPage} fluid />
           </Col>
-          <Col sm={4} style={{ paddingTop: "4vw" }}>
+          <Col md={4} style={{ paddingTop: "4vw" }}>
             <Row className="name">MERILL</Row>
             <Row className="name">BOBOTIS</Row>
             <Row className="bar" />
             <Row className="profession">Visual Artist</Row>
           </Col>
           <Col
-            sm={3}
+            md={3}
             style={{
               paddingLeft: "17vw",
               paddingTop: "2vw",
