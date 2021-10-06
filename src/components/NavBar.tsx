9d2c7c6782c28035e3b7b4761e736bd29940e0bb
IsaacMeadowcroft <54515271+IsaacMeadowcroft@users.noreply.github.com>
2021-10-06 17:36:07 -0400

    fixed logo error

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../css/NavBar.css";
 import { Container, Nav, Navbar } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function NavBar(): JSX.Element {
+  const [dimensions, setDimensions] = React.useState({
+    height: window.innerHeight,
+    width: window.innerWidth,
+  });
+
+  function handleResize() {
+    setDimensions({
+      height: window.innerHeight,
+      width: window.innerWidth,
+    });
+    console.log(dimensions.width);
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+  }, []);
+  
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
-        <Nav className="me-auto nav-styles" >
+        <Nav className={ dimensions.width <= 421? "me-auto nav-styles-sm" : "me-auto nav-styles-md" }>
           <Nav.Link href="#Home">HOME</Nav.Link>
           <Nav.Link href="#Prints">PRINTS</Nav.Link>
           <Nav.Link href="#Portfolio">PORTFOLIO</Nav.Link>
