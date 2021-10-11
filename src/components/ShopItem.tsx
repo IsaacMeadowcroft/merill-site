bbc47afb9eb7e07750deb33fbe5b1ff791b3e426
IsaacMeadowcroft <54515271+IsaacMeadowcroft@users.noreply.github.com>
2021-10-10 22:35:30 -0400

    onHover items

--- src/components/ShopItem.tsx
+++ src/components/ShopItem.tsx
@@ -1,19 +1,36 @@
-import React from "react";
-import '../css/ShopItem.css';
-import { Card, Button} from 'react-bootstrap';
+import React, { useState } from "react";
+import "../css/ShopItem.css";
+import { Card, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { CartItemType } from "./Prints";
+import LandingPage from "../assets/LandingPage.jpg";
 
 function ShopItem(itemData: CartItemType): JSX.Element {
+  const [isHovering, setIsHovering] = useState(false);
+
+  const handleMouseOver = () => {
+    setIsHovering(true);
+  };
+
+  const handleMouseOut = () => {
+    setIsHovering(false);
+  };
+
   return (
-    <Card style={{ width: "18rem", backgroundColor: "black", borderWidth: "1px", borderColor: "white", color: "white" }}>
-      <Card.Img variant="top" src={itemData.image} />
-      <Card.Body>
-        <Card.Title>{itemData.title}</Card.Title>
-        <Card.Text>{itemData.description.substring(0,50)}</Card.Text>
-        <Button variant="dark">{itemData.price}</Button>
-      </Card.Body>
-    </Card>
+    <div onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
+      <Card className="card-styles">
+          <Card.Img variant="top"  src={LandingPage} style={isHovering? {opacity: "20%"} : {opacity: "100%"}}/>
+        {isHovering ? (
+          <Card.ImgOverlay>
+            <Card.Title>{itemData.title}</Card.Title>
+            <Card.Text>{itemData.description.substring(0, 50)}</Card.Text>
+            <Button variant="dark">{itemData.price}</Button>
+          </Card.ImgOverlay>
+        ) : (
+          <></>
+        )}
+      </Card>
+    </div>
   );
 }
 
