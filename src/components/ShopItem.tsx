42cd314bb6412626b916ce2783f6aeeea4a7b198
IsaacMeadowcroft <54515271+IsaacMeadowcroft@users.noreply.github.com>
2021-10-13 11:20:43 -0400

    First Modal on ShopItem click

--- src/components/ShopItem.tsx
+++ src/components/ShopItem.tsx
@@ -1,20 +1,60 @@
 import React, { useState } from "react";
 import "../css/ShopItem.css";
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Modal, ModalProps } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { CartItemType } from "./Prints";
 import LandingPage from "../assets/LandingPage.jpg";
+import { Omit, BsPrefixProps } from "react-bootstrap/esm/helpers";
+
+function MyVerticallyCenteredModal(props: JSX.IntrinsicAttributes & Omit<Pick<React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>, "key" | keyof React.HTMLAttributes<HTMLDivElement>> & { ref?: ((instance: HTMLDivElement | null) => void) | React.RefObject<HTMLDivElement> | null | undefined; }, BsPrefixProps<"div"> & ModalProps> & BsPrefixProps<"div"> & ModalProps & { children?: React.ReactNode; }) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Modal heading
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4>Centered Modal</h4>
+        <p>
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+          consectetur ac, vestibulum at eros.
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
 
 function ShopItem(itemData: CartItemType): JSX.Element {
   const [isHovering, setIsHovering] = useState(false);
+  const [modalShow, setModalShow] = React.useState(false);
 
   return (
-      <Card className="card-styles" onMouseOver={() => {
-      setIsHovering(true);
-    }} onMouseLeave={() => {
-      setIsHovering(false);
-    }}>
-          <Card.Img variant="top"  src={LandingPage} style={isHovering? {filter: "brightness(20%)"} : {filter: "brightness(100%)"}}/>
+    <>
+      <Card
+        className="card-styles"
+        onMouseOver={() => setIsHovering(true)}
+        onMouseLeave={() => setIsHovering(false)}
+        onClick={() => setModalShow(true)}
+      >
+        <Card.Img
+          variant="top"
+          src={LandingPage}
+          style={
+            isHovering
+              ? { filter: "brightness(20%)" }
+              : { filter: "brightness(100%)" }
+          }
+        />
         {isHovering ? (
           <Card.ImgOverlay>
             <Card.Title>{itemData.title}</Card.Title>
@@ -25,6 +65,12 @@ function ShopItem(itemData: CartItemType): JSX.Element {
           <></>
         )}
       </Card>
+
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+    </>
   );
 }
 
