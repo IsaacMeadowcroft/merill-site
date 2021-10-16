5a260f7bbfca5fd06d4be3b9915e22d9f82c7153
IsaacMeadowcroft <54515271+IsaacMeadowcroft@users.noreply.github.com>
2021-10-16 12:38:10 -0400

    Remove undefined object from cart

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -11,7 +11,7 @@ import {
   Button,
 } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { IWindowShopCartProps, Size, CartItem} from "./Interfaces";
+import { IWindowShopCartProps, Size, CartItem } from "./Interfaces";
 
 function Cart(props: IWindowShopCartProps): JSX.Element {
   return (
@@ -24,40 +24,55 @@ function Cart(props: IWindowShopCartProps): JSX.Element {
         </Container>
       </Navbar>
       <Container fluid id="Cart">
-        {Array.from(props.cartItems.keys()).map((item) => (
-          <Card style={{marginTop: "5vh"}}>
-          <Card.Header>{props.shopItems?.find((x) => x.id === item.id)?.title}</Card.Header>
-          <Card.Body>
-            <Row style={{ padding: "0px", margin: "0px" }}>
-            <Col sm={3} style={{ padding: "0px", margin: "0px"}}>
-              <Image style={{maxWidth: "90%"}}
-                src={props.shopItems?.find((x) => x.id === item.id)?.image}
-              />
-            </Col>
-            <Col sm={9} style={{ padding: "0px", margin: "0px" }}>
-              <h5>ID: {item.id}</h5>
-              <h5>Size: {Size[item.size]}</h5>
-              <h5>
-                Description:{" "}
-                {props.shopItems?.find((x) => x.id === item.id)?.description}
-              </h5>
-              <h5>
-                Price: {props.shopItems?.find((x) => x.id === item.id)?.price}
-              </h5>
-              {console.log(props.cartItems.get({id: item.id, size: item.size}))}
-              <h5>Quantity: {props.cartItems.get({id: item.id, size: item.size})}</h5>
-              <Row style={{ width: "100%" }}>
-                <Button
-                  onClick={() => props.removeCartItem(item.id, item.size)}
-                >
-                  Remove from Cart
-                </Button>
-              </Row>
-            </Col>
-          </Row>
-          </Card.Body>
-        </Card>
-        ))}
+        {Array.from(props.cartItems.keys()).map((item) =>
+          item.id ? (
+            <Card style={{ marginTop: "5vh" }}>
+              <Card.Header>
+                {props.shopItems?.find((x) => x.id === item.id)?.title}
+              </Card.Header>
+              <Card.Body>
+                <Row style={{ padding: "0px", margin: "0px" }}>
+                  <Col sm={3} style={{ padding: "0px", margin: "0px" }}>
+                    <Image
+                      style={{ maxWidth: "90%" }}
+                      src={
+                        props.shopItems?.find((x) => x.id === item.id)?.image
+                      }
+                    />
+                  </Col>
+                  <Col sm={9} style={{ padding: "0px", margin: "0px" }}>
+                    <h5>ID: {item.id}</h5>
+                    <h5>Size: {Size[item.size]}</h5>
+                    <h5>
+                      Description:{" "}
+                      {
+                        props.shopItems?.find((x) => x.id === item.id)
+                          ?.description
+                      }
+                    </h5>
+                    <h5>
+                      Price:{" "}
+                      {props.shopItems?.find((x) => x.id === item.id)?.price}
+                    </h5>
+                    <h5>
+                      Quantity:{" "}
+                      {props.cartItems.get({ id: item.id, size: item.size })}
+                    </h5>
+                    <Row style={{ width: "100%" }}>
+                      <Button
+                        onClick={() => props.removeCartItem(item.id, item.size)}
+                      >
+                        Remove from Cart
+                      </Button>
+                    </Row>
+                  </Col>
+                </Row>
+              </Card.Body>
+            </Card>
+          ) : (
+            <></>
+          )
+        )}
       </Container>
     </>
   );
