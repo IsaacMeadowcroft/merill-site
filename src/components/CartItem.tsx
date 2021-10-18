844d592dfc97b850922fa14809887a138171ee4a
IsaacMeadowcroft <54515271+IsaacMeadowcroft@users.noreply.github.com>
2021-10-18 09:56:57 -0400

    Added Size to cart Item

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -9,17 +9,14 @@ import { FiPlus, FiMinus } from "react-icons/fi";
 function CartItem(props: ICartItemProps): JSX.Element {
   return (
     <Row style={{ padding: "0px", backgroundColor: "white", margin: "1vh" }}>
-      <Col sm={2} style={{ padding: "0px", margin: "0px" }}>
+      <Col sm={1} style={{ padding: "0px", margin: "0px" }}>
         <Image
           style={{ maxWidth: "90%" }}
           src={props.shopItems?.find((x) => x.id === props.cartItem.id)?.image}
         />
       </Col>
-      <Col sm={4} className="position-relative">
-        <Container
-          className="position-absolute top-50 start-0 translate-middle-y"
-          style={{ textAlign: "center", width: "100%" }}
-        >
+      <Col sm={7} className="position-relative">
+        <Container className="position-absolute top-50 start-0 translate-middle-y">
           <div style={{ textAlign: "center", width: "100%" }}>
             {props.shopItems?.find((x) => x.id === props.cartItem.id)?.title}
           </div>
@@ -28,38 +25,34 @@ function CartItem(props: ICartItemProps): JSX.Element {
           </div>
         </Container>
       </Col>
-      <Col sm={3} className="position-relative">
+      <Col sm={1} className="position-relative">
         <div
           className="position-absolute top-50 start-0 translate-middle-y"
           style={{ width: "100%", textAlign: "center" }}
         >
-          <Row>
-            <Col>
-              <div
-                className="cart-item-button"
-                onClick={() =>
-                  props.addCartItem(props.cartItem.id, props.cartItem.size)
-                }
-              >
-                <FiPlus />
-              </div>
+            <Row>
+            <Col sm={4}
+              className="cart-item-button"
+              onClick={() =>
+                props.addCartItem(props.cartItem.id, props.cartItem.size)
+              }
+            >
+              <FiPlus />
             </Col>
-            <Col>
-              <Badge bg="light" text="dark">
-                {props.quantity}
-              </Badge>
+            <Col sm={4}>
+            <Badge bg="light" text="dark">
+              {props.quantity}
+            </Badge>
             </Col>
-            <Col>
-              <div
-                className="cart-item-button"
-                onClick={() =>
-                  props.minusCartItem(props.cartItem.id, props.cartItem.size)
-                }
-              >
-                <FiMinus />
-              </div>
+            <Col sm={4}
+              className="cart-item-button"
+              onClick={() =>
+                props.minusCartItem(props.cartItem.id, props.cartItem.size)
+              }
+            >
+              <FiMinus />
             </Col>
-          </Row>
+            </Row>
         </div>
       </Col>
       <Col
