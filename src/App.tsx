1bf06b6ca6ce29524caadb6bbad58a9b00622fbd
IsaacMeadowcroft <54515271+IsaacMeadowcroft@users.noreply.github.com>
2021-10-21 17:28:07 -0400

    Pulling data from backend!

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ import { Spinner } from "react-bootstrap";
 import { TShopItem, Size } from "./components/Interfaces";
 
 const getProducts = async (): Promise<TShopItem[]> =>
-  await (await fetch("https://fakestoreapi.com/products")).json();
+  await (await fetch("http://127.0.0.1:8080/", )).json();
 
 function App(): JSX.Element {
   const [scrollPosition, setScrollPosition] = useState(0);
