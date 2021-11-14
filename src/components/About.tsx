c043c385c1018df120560575e123f2139b0a5d5f
IsaacMeadowcroft <54515271+IsaacMeadowcroft@users.noreply.github.com>
2021-11-14 18:32:31 -0500

    changed brightness of about pic

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -10,16 +10,23 @@ function About(props: IWindowProps): JSX.Element {
       fluid
       className="bg-black p-0 text-center text-white pt-5"
       style={{ fontFamily: '"Cormorant", serif' }}
+      id="About"
     >
       <h3>A B O U T</h3>
-      <Row md={2} className="m-0">
-        <Col className="w-50 p-0">
-          {
-            'Montreal-based, 18 year old freelance Visual Artist & Cognitive Science student at McGill University. ​ "The Artistic Process forces one’s vulnerability to surface, creating an incredibly intense connection between the artist and the subject, rarely found elsewhere. That vulnerability is what I try to capture the most. There is so much left unsaid, so many words hidden behind others… but body language does not lie. I usually go into a creative session with a vague idea of what I would like to create, and base the direction on what the subject is subconsciously communicating to me. Nothing has felt as genuine to me before."'
-          }
+      <Row md={2} className="m-0 d-flex column">
+        <Col className="w-50 p-5 h-100 align-self-center">
+          <h4>
+            {"Montreal-based, 20 year old freelance Visual Artist.\n"}
+            {
+              '"The Artistic Process forces one’s vulnerability to surface, creating an incredibly intense connection between the artist and the subject, rarely found elsewhere. That vulnerability is what I try to capture the most. There is so much left unsaid, so many words hidden behind others… but body language does not lie. I usually go into a creative session with a vague idea of what I would like to create, and base the direction on what the subject is subconsciously communicating to me. Nothing has felt as genuine to me before."'
+            }
+          </h4>
         </Col>
-        <Col className="w-50 p-0">
-          <img style={{ maxWidth: "30vw" }} src={AboutPage}></img>
+        <Col className="w-50">
+          <img
+            style={{ maxWidth: "30vw", filter: "brightness(40%)" }}
+            src={AboutPage}
+          ></img>
         </Col>
       </Row>
     </Container>
