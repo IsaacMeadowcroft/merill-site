7d323eb84477eb7ba4737eb98e2d044509e51e29
IsaacMeadowcroft <54515271+IsaacMeadowcroft@users.noreply.github.com>
2021-11-15 10:41:51 -0500

    fixed footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ function Footer(): JSX.Element {
     <footer className="site-footer">
       <div className="container">
         <div className="row">
-          <div className="col-sm-12 col-md-6">
+          <div className="col-sm-12 col-md-9">
             <h6>Contact Me</h6>
             <p className="text-justify">
               Scanfcode.com <i>CODE WANTS TO BE SIMPLE </i> is an initiative to
@@ -17,38 +17,7 @@ function Footer(): JSX.Element {
               Bootstrap, JavaScript, PHP, Android, SQL and Algorithm.
             </p>
           </div>
-
-          <div className="col-xs-6 col-md-3">
-            <h6>Social</h6>
-            <ul className="footer-links">
-              <li>
-                <a href="http://scanfcode.com/category/c-language/">C</a>
-              </li>
-              <li>
-                <a href="http://scanfcode.com/category/front-end-development/">
-                  UI Design
-                </a>
-              </li>
-              <li>
-                <a href="http://scanfcode.com/category/back-end-development/">
-                  PHP
-                </a>
-              </li>
-              <li>
-                <a href="http://scanfcode.com/category/java-programming-language/">
-                  Java
-                </a>
-              </li>
-              <li>
-                <a href="http://scanfcode.com/category/android/">Android</a>
-              </li>
-              <li>
-                <a href="http://scanfcode.com/category/templates/">Templates</a>
-              </li>
-            </ul>
-          </div>
-
-          <div className="col-xs-6 col-md-3">
+          <div className="col-sm-12 col-md-3">
             <h6>Quick Links</h6>
             <ul className="footer-links">
               <li>
