bff911b5e06fa17ad5020f6c36ee09bb809c22e3
IsaacMeadowcroft <54515271+IsaacMeadowcroft@users.noreply.github.com>
2021-11-14 19:48:59 -0500

    Added Portfolio section

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Carousel, Container } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { IWindowProps } from "./Interfaces";
+import LandingPage from "../assets/LandingPage.png";
 
 function Portfolio(props: IWindowProps): JSX.Element {
   return (
@@ -9,8 +10,50 @@ function Portfolio(props: IWindowProps): JSX.Element {
       fluid
       className="bg-black p-0 text-center text-white pt-5"
       style={{ fontFamily: '"Cormorant", serif' }}
+      id="Portfolio"
     >
       <h3>P O R T F O L I O</h3>
+      <div className="d-flex row justify-content-center">
+        <Carousel fade style={{ width: "35%" }}>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={LandingPage}
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={LandingPage}
+              alt="Second slide"
+            />
+
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src={LandingPage}
+              alt="Third slide"
+            />
+
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </div>
     </Container>
   );
 }
