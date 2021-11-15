d99515adba3a37f9d60128acc79959af60edbdda
IsaacMeadowcroft <54515271+IsaacMeadowcroft@users.noreply.github.com>
2021-11-14 19:41:13 -0500

    finished formatting about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,6 +3,7 @@ import { Col, Container, Row } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { IWindowProps } from "./Interfaces";
 import AboutPage from "../assets/AboutPage.png";
+import HeadShot from "../assets/AboutPageHeadShot.png";
 
 function About(props: IWindowProps): JSX.Element {
   return (
@@ -13,19 +14,22 @@ function About(props: IWindowProps): JSX.Element {
       id="About"
     >
       <h3>A B O U T</h3>
-      <Row md={2} className="m-0 d-flex column">
-        <Col className="w-50 p-5 h-100 align-self-center">
-          <h4>
+      <Row md={2} className="m-0">
+        <Col className="w-50 d-flex column justify-content-center">
+          <h4 className="px-4 align-self-center">
             {"Montreal-based, 20 year old freelance Visual Artist.\n"}
             {
               '"The Artistic Process forces one’s vulnerability to surface, creating an incredibly intense connection between the artist and the subject, rarely found elsewhere. That vulnerability is what I try to capture the most. There is so much left unsaid, so many words hidden behind others… but body language does not lie. I usually go into a creative session with a vague idea of what I would like to create, and base the direction on what the subject is subconsciously communicating to me. Nothing has felt as genuine to me before."'
             }
           </h4>
         </Col>
-        <Col className="w-50">
+        <Col className="d-flex row justify-content-center w-50 py-5">
           <img
-            style={{ maxWidth: "30vw", filter: "brightness(40%)" }}
-            src={AboutPage}
+            style={{
+              maxWidth: "60%",
+              borderRadius: "50%",
+            }}
+            src={HeadShot}
           ></img>
         </Col>
       </Row>
