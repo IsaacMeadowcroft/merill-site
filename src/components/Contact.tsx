257c157f335a68f4d20585a95d1e7cb4931169f2
IsaacMeadowcroft <54515271+IsaacMeadowcroft@users.noreply.github.com>
2021-11-16 10:08:50 -0500

    Added Contact section

---
+++ src/components/Contact.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { IWindowProps } from "./Interfaces";
+import HeadShot from "../assets/AboutPageHeadShot.png";
+
+function Contact(props: IWindowProps): JSX.Element {
+  return (
+    <Container
+      fluid
+      className="bg-black p-0 text-center text-white m-0"
+      style={{ fontFamily: '"Cormorant", serif' }}
+      id="About"
+    >
+      <Container fluid className="bg-black p-0 text-center text-white">
+        <h3>
+          <b>C O N T A C T</b>
+        </h3>
+      </Container>
+
+      <Row md={2} className="m-0">
+        <Col className="w-50 d-flex column justify-content-center">
+          <h4 className="px-4 align-self-center">
+            {
+              "Merill Bobotis is a Montreal-based, 20 year old freelance Visual Artist."
+            }
+            {
+              '"The Artistic Process forces one’s vulnerability to surface, creating an incredibly intense connection between the artist and the subject, rarely found elsewhere. That vulnerability is what I try to capture the most. There is so much left unsaid, so many words hidden behind others… but body language does not lie. I usually go into a creative session with a vague idea of what I would like to create, and base the direction on what the subject is subconsciously communicating to me. Nothing has felt as genuine to me before."'
+            }
+          </h4>
+        </Col>
+        <Col className="d-flex row justify-content-center w-50 py-5">
+          <img
+            style={{
+              maxWidth: "60%",
+              borderRadius: "50%",
+            }}
+            src={HeadShot}
+          ></img>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Contact;
