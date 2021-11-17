aa522e3ec98c9445f1f53b20048b6a959faac5b7
IsaacMeadowcroft <54515271+IsaacMeadowcroft@users.noreply.github.com>
2021-11-17 16:30:31 -0500

    chnaged cart nav

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,63 +1,53 @@
 import React from "react";
 import "../css/Cart.css";
-import { Col, Container, Nav, Row } from "react-bootstrap";
+import { Container, Nav, Navbar } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { TCartItem, IWindowShopCartProps } from "./Interfaces";
 import CartItem from "./CartItem";
-import Checkout from "./Checkout";
-import { VscArrowLeft } from "react-icons/vsc";
+import { VscArrowLeft, VscArrowRight } from "react-icons/vsc";
 
 function Cart(props: IWindowShopCartProps): JSX.Element {
   return (
-    <Container fluid>
-      <Row style={{ backgroundColor: "whitesmoke"}}>
-        <Col sm={8}>
-          <Nav.Link href="/merill-site/">
-            <h4>
-              <VscArrowLeft /> Continue Shopping
-            </h4>
+    <div style={{ fontFamily: '"Cormorant", serif', fontSize: "2.1vh" }}>
+      <Navbar expand="lg" variant="dark" bg="dark" sticky="top">
+        <Container fluid className="d-flex flex-row justify-content-between">
+          <Nav.Link href="/merill-site/" className="text-light">
+            <VscArrowLeft /> B A C K
           </Nav.Link>
-          <Row style={{ backgroundColor: "whitesmoke"}}>
-            {Array.from(props.cartItems.keys()).map((item) => {
-              let cartItem: TCartItem;
-              const quantity = props.cartItems.get(item);
-              try {
-                cartItem = JSON.parse(item);
-              } catch (err) {
-                return <></>;
-              }
-              if (quantity) {
-                return (
-                  <CartItem
-                    quantity={quantity}
-                    cartItem={cartItem}
-                    shopItems={props.shopItems}
-                    dimensions={props.dimensions}
-                    scrollPosition={props.scrollPosition}
-                    addCartItem={props.addCartItem}
-                    minusCartItem={props.minusCartItem}
-                    removeCartItem={props.removeCartItem}
-                  />
-                );
-              } else {
-                return <div style={{ backgroundColor: "whitesmoke"}}></div>;
-              }
-            })}
-          </Row>
-        </Col>
-        <Col sm={4} style={{ height: "100vh", backgroundColor: "whitesmoke", position: "fixed", right: "0px", top: "0px"  }}>
-          <Checkout
-            cartItems={props.cartItems}
-            shopItems={props.shopItems}
-            dimensions={props.dimensions}
-            scrollPosition={props.scrollPosition}
-            addCartItem={props.addCartItem}
-            minusCartItem={props.minusCartItem}
-            removeCartItem={props.removeCartItem}
-          />
-        </Col>
-      </Row>
-    </Container>
+          <Nav.Link href="/merill-site/" className="text-light">
+            C H E C K O U T
+            <VscArrowRight />
+          </Nav.Link>
+        </Container>
+      </Navbar>
+      <Container fluid className="bg-light">
+        {Array.from(props.cartItems.keys()).map((item) => {
+          let cartItem: TCartItem;
+          const quantity = props.cartItems.get(item);
+          try {
+            cartItem = JSON.parse(item);
+          } catch (err) {
+            return <></>;
+          }
+          if (quantity) {
+            return (
+              <CartItem
+                quantity={quantity}
+                cartItem={cartItem}
+                shopItems={props.shopItems}
+                dimensions={props.dimensions}
+                scrollPosition={props.scrollPosition}
+                addCartItem={props.addCartItem}
+                minusCartItem={props.minusCartItem}
+                removeCartItem={props.removeCartItem}
+              />
+            );
+          } else {
+            return <div style={{ backgroundColor: "whitesmoke" }}></div>;
+          }
+        })}
+      </Container>
+    </div>
   );
 }
 
