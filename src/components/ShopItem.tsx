f3f2247b6b51bbab8b2477a38568ed069fd63fdb
IsaacMeadowcroft <54515271+IsaacMeadowcroft@users.noreply.github.com>
2021-11-17 11:40:33 -0500

    changed cards

--- src/components/ShopItem.tsx
+++ src/components/ShopItem.tsx
@@ -6,7 +6,6 @@ import ItemModal from "./ItemModal";
 import { IShopItemProps, Size } from "./Interfaces";
 
 function ShopItem(props: IShopItemProps): JSX.Element {
-  const [isHovering, setIsHovering] = useState(false);
   const [modalShow, setModalShow] = useState(false);
   const shopItemTitleSubString = props.shopItem.title.substring(0, 35);
   const shopItemDescriptionSubString = props.shopItem.description.substring(
@@ -18,48 +17,48 @@ function ShopItem(props: IShopItemProps): JSX.Element {
     <>
       <Card
         className="card-styles border-0 rounded-0"
-        onMouseOver={() => setIsHovering(true)}
-        onMouseLeave={() => setIsHovering(false)}
         onClick={() => setModalShow(true)}
       >
         <Card.Img
           variant="top"
           src={props.shopItem.image}
           className="rounded-0"
-          style={
-            isHovering
-              ? { filter: "brightness(20%)" }
-              : { filter: "brightness(100%)" }
-          }
         />
-        {isHovering ? (
-          <Card.ImgOverlay className="d-flex flex-column justify-content-between">
-            <div>
-              <Card.Title>
-                {shopItemTitleSubString}
-                {shopItemTitleSubString != props.shopItem.title ? "..." : ""}
-              </Card.Title>
-              <Card.Text>
-                {shopItemDescriptionSubString}
-                {shopItemDescriptionSubString != props.shopItem.description
-                  ? "..."
-                  : ""}
-              </Card.Text>
-              <h6>${props.shopItem.price}</h6>
-            </div>
-            <div className="w-100 align-self-end">
+        <Card.Body className="d-flex flex-column justify-content-between">
+          <div>
+            <Card.Title>
+              {shopItemTitleSubString}
+              {shopItemTitleSubString != props.shopItem.title ? "..." : ""}
+            </Card.Title>
+            <Card.Text>
+              {shopItemDescriptionSubString}
+              {shopItemDescriptionSubString != props.shopItem.description
+                ? "..."
+                : ""}
+            </Card.Text>
+            <h6>${props.shopItem.price}</h6>
+          </div>
+          <div className="w-100 align-self-end mt-3">
+            <div className="d-flex column justify-content-between">
               <Button
                 variant="outline-light"
-                className=" py-1 px-3 text-italic"
+                className=" py-1 px-3 text-italic w-50"
+                style={{ marginRight: "1%" }}
+                onClick={() => props.addCartItem(props.shopItem.id, Size.SMALL)}
+              >
+                Add to Cart
+              </Button>
+              <Button
+                variant="outline-light"
+                className=" py-1 px-3 text-italic w-50"
+                style={{ marginLeft: "1%" }}
                 onClick={() => props.addCartItem(props.shopItem.id, Size.SMALL)}
               >
                 Buy
               </Button>
             </div>
-          </Card.ImgOverlay>
-        ) : (
-          <></>
-        )}
+          </div>
+        </Card.Body>
       </Card>
 
       <ItemModal
