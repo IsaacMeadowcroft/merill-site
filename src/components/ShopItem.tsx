f18a90b85399580b511d5a58fec7a806fa3d4f7f
IsaacMeadowcroft <54515271+IsaacMeadowcroft@users.noreply.github.com>
2021-11-17 22:33:27 -0500

    finished card

--- src/components/ShopItem.tsx
+++ src/components/ShopItem.tsx
@@ -115,53 +115,81 @@ function ShopItem(props: IShopItemProps): JSX.Element {
                 ? "..."
                 : ""}
               </Card.Text>*/}
-          <div className="w-100" style={{ textTransform: "uppercase" }}>
-            <b>
-              {shopItemTitleSubString}
-              {shopItemTitleSubString != props.shopItem.title ? "..." : ""}
-            </b>
-          </div>
-
-          <div className="w-100 d-flex flex-row justify-content-between">
-            <div className="d-flex flex-row justify-content-between">
-              <div>S &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div>
-              <div>M &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div>
-              <div>L &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div>
+          <Card.Title style={{ textTransform: "uppercase" }}>
+            {shopItemTitleSubString}
+            {shopItemTitleSubString != props.shopItem.title ? "..." : ""}
+          </Card.Title>
+          <Card.Subtitle>
+            <div className="w-100 d-flex flex-row justify-content-between">
+              <div className="d-flex w-25 flex-row justify-content-between">
+                <b
+                  className={
+                    "size " +
+                    (currentItemSize == Size.SMALL
+                      ? "size-active"
+                      : "size-inactive")
+                  }
+                  onClick={() => setItemSize(Size.SMALL)}
+                >
+                  S
+                </b>
+                <b
+                  className={
+                    "size " +
+                    (currentItemSize == Size.MEDIUM
+                      ? "size-active"
+                      : "size-inactive")
+                  }
+                  onClick={() => setItemSize(Size.MEDIUM)}
+                >
+                  M
+                </b>
+                <b
+                  className={
+                    "size " +
+                    (currentItemSize == Size.LARGE
+                      ? "size-active"
+                      : "size-inactive")
+                  }
+                  onClick={() => setItemSize(Size.LARGE)}
+                >
+                  L
+                </b>
+              </div>
+              <b>${currentItemPrice}</b>
             </div>
-            <div>${currentItemPrice}</div>
-          </div>
+          </Card.Subtitle>
 
-          <div className="w-100 d-flex flex-row justify-content-between">
+          {/*<div className="w-100 d-flex flex-row justify-content-between">
             <div>Checkout</div>
             <div>Add to cart</div>
-          </div>
+            </div>*/}
 
-          {/*
           <div className="w-100 align-self-end mt-3">
             <div className="d-flex column justify-content-between">
               <Button
-                variant="outline-light"
+                variant="dark"
                 className=" py-1 px-3 text-italic w-50"
-                style={{ marginRight: "1%" }}
+                style={{ marginRight: "2%" }}
                 onClick={() => {
                   props.addCartItem(props.shopItem.id, currentItemSize);
                 }}
               >
-                Add to Cart
+                Add to cart
               </Button>
               <Button
-                variant="outline-light"
+                variant="dark"
                 className=" py-1 px-3 text-italic w-50"
-                style={{ marginLeft: "1%" }}
+                style={{ marginLeft: "2%" }}
                 onClick={() => {
                   props.addCartItem(props.shopItem.id, currentItemSize);
                   history.push("/merill-site/Cart");
                 }}
               >
-                Buy: ${currentItemPrice}
+                Buy now
               </Button>
             </div>
-              </div>*/}
+          </div>
         </Card.Body>
       </Card>
 
