c10d747abe6ce3d28b2d880ff3b611dd91203b18
IsaacMeadowcroft <54515271+IsaacMeadowcroft@users.noreply.github.com>
2021-11-26 17:54:49 -0500

    Removed unecessary code

--- src/components/ShopItem.tsx
+++ src/components/ShopItem.tsx
@@ -1,35 +1,16 @@
 import React, { useState } from "react";
 import "../css/ShopItem.css";
-import { Button, Card, Carousel, Form, Toast } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BsFillBagFill } from "react-icons/bs";
 import ItemModal from "./ItemModal";
 import { IShopItemProps, Size } from "./Interfaces";
 import { useHistory } from "react-router-dom";
-import PictureFrame from "../assets/PictureFrame.jpg";
-import PictureFrame2 from "../assets/PictureFrame2.jpg";
 
 function ShopItem(props: IShopItemProps): JSX.Element {
   const history = useHistory();
   const [currentItemSize, setItemSize] = useState<Size>(Size.SMALL);
   const [modalShow, setModalShow] = useState(false);
   const shopItemTitleSubString = props.shopItem.title.substring(0, 35);
-  const shopItemDescriptionSubString = props.shopItem.description.substring(
-    0,
-    85
-  );
-
-  const handleSubmitSmall = () => {
-    setItemSize(Size.SMALL);
-  };
-
-  const handleSubmitMedium = () => {
-    setItemSize(Size.MEDIUM);
-  };
-
-  const handleSubmitLarge = () => {
-    setItemSize(Size.LARGE);
-  };
 
   let currentItemPrice = props.shopItem.price;
   if (currentItemSize == Size.SMALL) {
