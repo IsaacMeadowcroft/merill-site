41f1367f0e8f4600bc5280e7a63bb625dd83d4c2
IsaacMeadowcroft <54515271+IsaacMeadowcroft@users.noreply.github.com>
2021-11-26 20:12:28 -0500

    changed about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -25,14 +25,24 @@ function About(props: IWindowProps): JSX.Element {
 
         <Row md={2} className="m-0">
           <Col className="w-50 d-flex column justify-content-center">
-            <h4 className="px-4 align-self-center">
-              {
-                "Merill Bobotis is a Montreal-based, 20 year old freelance Visual Artist."
-              }
-              {
-                '"The Artistic Process forces one’s vulnerability to surface, creating an incredibly intense connection between the artist and the subject, rarely found elsewhere. That vulnerability is what I try to capture the most. There is so much left unsaid, so many words hidden behind others… but body language does not lie. I usually go into a creative session with a vague idea of what I would like to create, and base the direction on what the subject is subconsciously communicating to me. Nothing has felt as genuine to me before."'
-              }
-            </h4>
+            <h5
+              className="px-4 align-self-center"
+              style={{ fontWeight: "lighter" }}
+            >
+              <div>
+                Merill Bobotis is a Montreal-based, 20 year old freelance Visual
+                Artist. <br /> <br />
+                "The Artistic Process forces one’s vulnerability to surface,
+                creating an incredibly intense connection between the artist and
+                the subject, rarely found elsewhere. That vulnerability is what
+                I try to capture the most. There is so much left unsaid, so many
+                words hidden behind others… but body language does not lie. I
+                usually go into a creative session with a vague idea of what I
+                would like to create, and base the direction on what the subject
+                is subconsciously communicating to me. Nothing has felt as
+                genuine to me before.
+              </div>
+            </h5>
           </Col>
           <Col className="d-flex row justify-content-center w-50 py-5">
             <div>
