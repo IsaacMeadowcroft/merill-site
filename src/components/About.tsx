258e93f0d7cca656b75345887eaf414536f9c6c4
IsaacMeadowcroft <54515271+IsaacMeadowcroft@users.noreply.github.com>
2021-11-27 12:54:31 -0500

    made about page responsive

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -23,38 +23,82 @@ function About(props: IWindowProps): JSX.Element {
           </h3>
         </Container>
 
-        <Row md={2} className="m-0">
-          <Col className="w-50 d-flex column justify-content-center">
-            <h5
-              className="px-4 align-self-center"
-              style={{ fontWeight: "lighter" }}
-            >
-              <div>
-                Merill Bobotis is a Montreal-based, 20 year old freelance Visual
-                Artist. <br /> <br />
-                "The Artistic Process forces one’s vulnerability to surface,
-                creating an incredibly intense connection between the artist and
-                the subject, rarely found elsewhere. That vulnerability is what
-                I try to capture the most. There is so much left unsaid, so many
-                words hidden behind others… but body language does not lie. I
-                usually go into a creative session with a vague idea of what I
-                would like to create, and base the direction on what the subject
-                is subconsciously communicating to me. Nothing has felt as
-                genuine to me before.
-              </div>
-            </h5>
-          </Col>
-          <Col className="d-flex row justify-content-center w-50 py-5">
-            <div>
-              <img
-                style={{
-                  maxWidth: "60%",
-                  borderRadius: "50%",
-                }}
-                src={HeadShot}
-              ></img>
-            </div>
-          </Col>
+        <Row className="m-0 pb-5">
+          {props.dimensions.width >= 768 ? (
+            <>
+              {" "}
+              <Col md={7} className="d-flex column justify-content-center">
+                <div
+                  className="px-4 align-self-center"
+                  style={{ fontWeight: "lighter", fontSize: "2.8vh" }}
+                >
+                  <div>
+                    Merill Bobotis is a Montreal-based, 20 year old freelance
+                    Visual Artist. <br /> <br />
+                    "The Artistic Process forces one’s vulnerability to surface,
+                    creating an incredibly intense connection between the artist
+                    and the subject, rarely found elsewhere. That vulnerability
+                    is what I try to capture the most. There is so much left
+                    unsaid, so many words hidden behind others… but body
+                    language does not lie. I usually go into a creative session
+                    with a vague idea of what I would like to create, and base
+                    the direction on what the subject is subconsciously
+                    communicating to me. Nothing has felt as genuine to me
+                    before.
+                  </div>
+                </div>
+              </Col>
+              <Col md={5} className="d-flex flex-column justify-content-center">
+                <div className="d-flex flex-row justify-content-center py-3">
+                  <img
+                    style={{
+                      maxWidth: "70%",
+                      borderRadius: "50%",
+                    }}
+                    src={HeadShot}
+                  ></img>
+                </div>
+              </Col>
+            </>
+          ) : (
+            <>
+              <Row
+                md={5}
+                className="d-flex flex-column justify-content-center m-0"
+              >
+                <div className="d-flex flex-row justify-content-center py-3">
+                  <img
+                    style={{
+                      maxWidth: "60%",
+                      borderRadius: "50%",
+                    }}
+                    src={HeadShot}
+                  ></img>
+                </div>
+              </Row>
+              <Row className="d-flex column justify-content-center m-0">
+                <div
+                  className="px-4 align-self-center"
+                  style={{ fontWeight: "lighter", fontSize: "2.8vh" }}
+                >
+                  <div>
+                    Merill Bobotis is a Montreal-based, 20 year old freelance
+                    Visual Artist. <br /> <br />
+                    "The Artistic Process forces one’s vulnerability to surface,
+                    creating an incredibly intense connection between the artist
+                    and the subject, rarely found elsewhere. That vulnerability
+                    is what I try to capture the most. There is so much left
+                    unsaid, so many words hidden behind others… but body
+                    language does not lie. I usually go into a creative session
+                    with a vague idea of what I would like to create, and base
+                    the direction on what the subject is subconsciously
+                    communicating to me. Nothing has felt as genuine to me
+                    before.
+                  </div>
+                </div>
+              </Row>
+            </>
+          )}
         </Row>
       </Container>
     </>
