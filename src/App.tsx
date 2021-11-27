00380f549be704db0328258114af9cb69290309b
IsaacMeadowcroft <54515271+IsaacMeadowcroft@users.noreply.github.com>
2021-11-27 17:12:58 -0500

    added fake data to sight

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,8 @@ function App(): JSX.Element {
   const [cartItems, setCartItems] = localStorageCartRequest
     ? useState(new Map<string, number>(JSON.parse(localStorageCartRequest)))
     : useState(new Map<string, number>());
-  const { data, isLoading, error } = useQuery<TShopItem[]>(
+  // eslint-disable-next-line prefer-const
+  let { data, isLoading, error } = useQuery<TShopItem[]>(
     "products",
     getProducts
   );
@@ -89,67 +90,146 @@ function App(): JSX.Element {
   }, [dimensions]);
 
   if (error) {
-    return <div>Something went wrong... </div>;
-  } else {
-    return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/merill-site">
-            <NavBar
-              dimensions={dimensions}
-              scrollPosition={scrollPosition}
-              cartItems={cartItems}
-            />
-            <Home dimensions={dimensions} scrollPosition={scrollPosition} />
-            <Portfolio
-              dimensions={dimensions}
-              scrollPosition={scrollPosition}
-            />
-            {isLoading ? (
-              <div
-                className="w-100 d-flex flex-row justify-content-center"
-                style={{ backgroundColor: "rgb(20,20,20)" }}
-              >
-                <Spinner animation="border" variant="light" role="status">
-                  <span className="visually-hidden">Loading...</span>
-                </Spinner>
-              </div>
-            ) : (
-              <Prints
-                dimensions={dimensions}
-                scrollPosition={scrollPosition}
-                shopItems={data}
-                cartItems={cartItems}
-                addCartItem={addCartItem}
-                minusCartItem={minusCartItem}
-                removeCartItem={removeCartItem}
-              />
-            )}
-            <Contact dimensions={dimensions} scrollPosition={scrollPosition} />
-            <About dimensions={dimensions} scrollPosition={scrollPosition} />
-            <Footer />
-          </Route>
-          <Route path="/merill-site/Cart">
-            {isLoading ? (
-              <Spinner animation="border" role="status">
-                <span className="visually-hidden">Loading...</span>
-              </Spinner>
-            ) : (
-              <Cart
-                dimensions={dimensions}
-                scrollPosition={scrollPosition}
-                shopItems={data}
-                cartItems={cartItems}
-                addCartItem={addCartItem}
-                minusCartItem={minusCartItem}
-                removeCartItem={removeCartItem}
-              />
-            )}
-          </Route>
-        </Switch>
-      </BrowserRouter>
+    data = new Array<TShopItem>();
+    data.push(
+      {
+        id: 1,
+        description: "...",
+        image:
+          "https://live.staticflickr.com/65535/51616325094_6ba145bc43_h_d.jpg",
+        price: 50,
+        title: "Cloak",
+      },
+      {
+        id: 2,
+        description: "...",
+        image:
+          "https://live.staticflickr.com/65535/51616325104_90be9f78b4_h_d.jpg",
+        price: 50,
+        title: "Blindfolded",
+      },
+      {
+        id: 3,
+        description: "...",
+        image:
+          "https://live.staticflickr.com/65535/51615890498_e168f54bda_h_d.jpg",
+        price: 50,
+        title: "Flowers",
+      },
+      {
+        id: 4,
+        description: "...",
+        image:
+          "https://live.staticflickr.com/65535/51615890538_c30f936353_o_d.jpg",
+        price: 50,
+        title: "Despair",
+      },
+      {
+        id: 5,
+        description: "...",
+        image:
+          "https://live.staticflickr.com/65535/51614834452_218a764dc0_o_d.jpg",
+        price: 50,
+        title: "Lake",
+      },
+      {
+        id: 6,
+        description: "...",
+        image:
+          "https://live.staticflickr.com/65535/51616537440_8be2d34228_o_d.jpg",
+        price: 50,
+        title: "Sin",
+      },
+      {
+        id: 7,
+        description: "...",
+        image:
+          "https://live.staticflickr.com/65535/51615668996_996edf8d43_o_d.jpg",
+        price: 50,
+        title: "Pain",
+      },
+      {
+        id: 8,
+        description: "...",
+        image:
+          "https://live.staticflickr.com/65535/51616325149_b3443969ea_o_d.jpg",
+        price: 50,
+        title: "Insight",
+      },
+      {
+        id: 9,
+        description: "...",
+        image:
+          "https://live.staticflickr.com/65535/51616325084_f45bb8d167_o_d.jpg",
+        price: 50,
+        title: "Freedom",
+      },
+      {
+        id: 9,
+        description: "...",
+        image:
+          "https://live.staticflickr.com/65535/51615669001_f560d30667_o_d.jpg",
+        price: 50,
+        title: "Horror",
+      }
     );
   }
+
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/merill-site">
+          <NavBar
+            dimensions={dimensions}
+            scrollPosition={scrollPosition}
+            cartItems={cartItems}
+          />
+          <Home dimensions={dimensions} scrollPosition={scrollPosition} />
+          <Portfolio dimensions={dimensions} scrollPosition={scrollPosition} />
+          {isLoading ? (
+            <div
+              className="w-100 d-flex flex-row justify-content-center"
+              style={{ backgroundColor: "rgb(20,20,20)" }}
+            >
+              <Spinner animation="border" variant="light" role="status">
+                <span className="visually-hidden">Loading...</span>
+              </Spinner>
+            </div>
+          ) : (
+            <Prints
+              dimensions={dimensions}
+              scrollPosition={scrollPosition}
+              shopItems={data}
+              cartItems={cartItems}
+              addCartItem={addCartItem}
+              minusCartItem={minusCartItem}
+              removeCartItem={removeCartItem}
+            />
+          )}
+          <Contact dimensions={dimensions} scrollPosition={scrollPosition} />
+          <About dimensions={dimensions} scrollPosition={scrollPosition} />
+          <Footer />
+        </Route>
+        <Route path="/merill-site/Cart">
+          {isLoading ? (
+            <Spinner animation="border" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </Spinner>
+          ) : (
+            <Cart
+              dimensions={dimensions}
+              scrollPosition={scrollPosition}
+              shopItems={data}
+              cartItems={cartItems}
+              addCartItem={addCartItem}
+              minusCartItem={minusCartItem}
+              removeCartItem={removeCartItem}
+            />
+          )}
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
